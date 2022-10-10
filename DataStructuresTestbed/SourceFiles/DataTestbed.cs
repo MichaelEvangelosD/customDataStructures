07af195b5ce62916a46ab0025376c1b63650f260
MichaelEvangelosD <13-13246@saeinstitute.edu>
2022-10-10 17:44:34 +0300

    Initial Project commit

---
+++ DataStructuresTestbed/SourceFiles/DataTestbed.cs
@@ -0,0 +1,17 @@
+﻿using DataStructures;
+
+namespace DataStructuresTestbed.SourceFiles
+{
+    public class DataTestbed
+    {
+        static void Main(string[] args)
+        {
+            new DataTestbed().DataTest();
+        }
+
+        void DataTest()
+        {
+            Console.Write("Hello");
+        }
+    }
+}
